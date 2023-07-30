93d2895ffa34f3134087403b63b23beca6c927ce
Nicolas Giaconia <73022145+nicolas-webdev@users.noreply.github.com>
2023-07-30 18:39:31 +0900

    Slight change of color for footer

--- app/(components)/Footer.tsx
+++ app/(components)/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="bg-slate-100 border-t border-accent-2">
+    <footer className="bg-stone-100 border-t border-accent-2">
       <div className="container mx-auto px-5">
         <div className="py-28 flex flex-col lg:flex-row items-center">
           <h3 className="text-4xl lg:text-5xl font-bold tracking-tighter leading-tight lg:leading-snug text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
