e29b9f76cee11285121a284c0d273673a5f6c726
Nicolas Giaconia <73022145+nicolas-webdev@users.noreply.github.com>
2023-07-30 17:22:05 +0900

    Clean PostBody code up

--- app/(components)/PostBody.tsx
+++ app/(components)/PostBody.tsx
@@ -1,76 +1,87 @@
-import { SITE_DOMAIN } from "@/config";
+import { SITE_HOSTNAME, SITE_URL } from "@/config";
 import Image from "next/image";
 import Link from "next/link";
+import { JSDOM } from "jsdom";
+import createDOMPurify from "dompurify";
 import parse, {
   HTMLReactParserOptions,
   Element,
   domToReact,
 } from "html-react-parser";
-import { JSDOM } from "jsdom";
-import createDOMPurify from "dompurify";
+
+// タグタイプの定数
+const TAG_TYPE = "tag";
+const IMG_TAG = "img";
+const A_TAG = "a";
+const HOSTNAME = SITE_HOSTNAME;
+
+// imgタグを処理するためのヘルパー関数
+const handleImageTag = (element: Element) => {
+  if (element.attribs?.src) {
+    // 画像にheightとwidthの属性がある場合、アスペクト比を維持
+    return element.attribs.height && element.attribs.width ? (
+      <Image
+        src={element.attribs.src}
+        alt={element.attribs.alt || ""}
+        width={parseInt(element.attribs.width)}
+        height={parseInt(element.attribs.height)}
+      />
+    ) : (
+      // 画像にheightやwidthの属性がない場合、layout fillを使用
+      <div className="relative aspect-video">
+        <Image
+          src={element.attribs.src}
+          alt={element.attribs.alt || ""}
+          className="object-contain"
+          layout="fill"
+        />
+      </div>
+    );
+  }
+};
+
+// aタグを処理するためのヘルパー関数
+const handleATag = (element: Element, options: HTMLReactParserOptions) => {
+  if (element.attribs?.href) {
+    const href = element.attribs.href;
+    const url = new URL(href, SITE_URL); // ベースURL
+    const linkText = element.children?.length
+      ? domToReact(element.children, options)
+      : null;
+
+    // リンクが内部の場合、Next.jsのLinkコンポーネントを使用
+    if (url.hostname === HOSTNAME) {
+      return <Link href={href}>{linkText}</Link>;
+    } else {
+      // リンクが外部の場合、target="_blank"を追加
+      return (
+        <a href={href} rel="noopener noreferrer" target="_blank">
+          {linkText}
+        </a>
+      );
+    }
+  }
+};
 
 const parseHTML = (htmlContent: string) => {
   const { window } = new JSDOM("");
-
-  // DOMPurifyを使用してHTMLをサニタイズする
+  // DOMPurifyを使用してHTMLをサニタイズ
   const DOMPurify = createDOMPurify(window);
   const clean = DOMPurify.sanitize(htmlContent);
 
-  // HTMLをパースする
   const options: HTMLReactParserOptions = {
     replace: (domNode) => {
-      if (domNode.type === "tag") {
+      if (domNode.type === TAG_TYPE) {
         const element = domNode as Element;
-
-        // imgタグの場合、Next.jsのImageコンポーネントを使用する
-        if (element.name === "img" && element.attribs && element.attribs.src) {
-          // widthとheightが指定されている場合、画像のアスペクト比を維持する
-          return element.attribs.height && element.attribs.width ? (
-            <Image
-              src={element.attribs.src}
-              alt={element.attribs.alt || ""}
-              width={parseInt(element.attribs.width)}
-              height={parseInt(element.attribs.height)}
-            />
-          ) : (
-            // widthとheightが指定されていない場合、layout fillを使用して画像を表示する
-            <div className="relative aspect-video">
-              <Image
-                src={element.attribs.src}
-                alt={element.attribs.alt || ""}
-                className="object-contain"
-                layout="fill"
-              />
-            </div>
-          );
-        }
-        if (element.name === "a" && element.attribs && element.attribs.href) {
-          const href = element.attribs.href;
-          const url = new URL(href, SITE_DOMAIN); // ベースURL
-          // 内部リンクの場合、Next.jsのLinkコンポーネントを使用する
-          if (url.hostname === "example.com") {
-            return (
-              <Link href={href}>
-                {element.children &&
-                  element.children.length > 0 &&
-                  domToReact(element.children, options)}
-              </Link>
-            );
-          } else {
-            // 外部リンクの場合、target="_blank"を付与する
-            return (
-              <a href={href} rel="noopener noreferrer" target="_blank">
-                {element.children &&
-                  element.children.length > 0 &&
-                  domToReact(element.children, options)}
-              </a>
-            );
-          }
-        }
+        // imgタグを処理
+        if (element.name === IMG_TAG) return handleImageTag(element);
+        // aタグを処理
+        if (element.name === A_TAG) return handleATag(element, options);
       }
     },
   };
 
+  // サニタイズしたHTMLをパース
   return parse(clean, options);
 };
 
@@ -78,11 +89,11 @@ type PostBodyProps = {
   htmlContent: string;
 };
 
-const PostBody = (props: PostBodyProps) => {
-  const { htmlContent } = props;
+// 投稿本文を表示するコンポーネント
+const PostBody = ({ htmlContent }: PostBodyProps) => {
   const parsedHtmlContents = parseHTML(htmlContent);
+  // TailwindCSSのproseプラグインを使用してブログコンテンツをスタイル付け
   return (
-    // TailwindCSSのproseプラグインを使用して、ブログのコンテンツをスタイルする
     <article className="prose prose-slate lg:prose-xl dark:prose-invert prose-img:rounded-sm prose-headings:underline prose-a:text-blue-600">
       {parsedHtmlContents}
     </article>
--- config.ts
+++ config.ts
@@ -1,4 +1,5 @@
-export const SITE_DOMAIN = process.env.SITE_DOMAIN || "http://localhost:3000";
+export const SITE_URL = process.env.SITE_URL || "http://localhost:3000";
+export const SITE_HOSTNAME = process.env.SITE_HOSTNAME || "example.com";
 export const MICROCMS_SERVICE_DOMAIN =
   process.env.MICROCMS_SERVICE_DOMAIN || "";
 export const MICROCMS_API_KEY = process.env.MICROCMS_API_KEY || "";
