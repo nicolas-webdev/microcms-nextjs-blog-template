e69ced9165f5790c03d9f6e48bad92412ce473d2
Nicolas Giaconia <73022145+nicolas-webdev@users.noreply.github.com>
2023-07-30 15:48:24 +0900

    Add types and utils for getting blog info

--- lib/microcms.ts
+++ lib/microcms.ts
@@ -4,7 +4,12 @@ import {
   MICROCMS_CATEGORIES_ENDPOINT,
   MICROCMS_SERVICE_DOMAIN,
 } from "@/config";
-import { BlogPost } from "@/types/microcms";
+import {
+  BlogPost,
+  BlogPostList,
+  Category,
+  CategoryList,
+} from "@/types/microcms";
 import { createClient } from "microcms-js-sdk";
 
 export const client = createClient({
@@ -17,12 +22,12 @@ const getContents = async (endpoint: string) => {
   return response.contents;
 };
 
-export const getBlogList = async (): Promise<BlogPost[]> => {
+export const getBlogList = async (): Promise<BlogPostList> => {
   const blogs = await getContents(MICROCMS_BLOGS_ENDPOINT);
   return blogs;
 };
 
-export const getCategoryList = async () => {
+export const getCategoryList = async (): Promise<CategoryList> => {
   const categories = await getContents(MICROCMS_CATEGORIES_ENDPOINT);
   return categories;
 };
@@ -36,6 +41,35 @@ export async function getBlogBySlug(slug: string): Promise<BlogPost> {
     return blogs.contents[0];
   } catch (error) {
     console.error("指定のブログの取得に失敗しました。", error);
+    throw error;
+  }
+}
+
+export async function getCategoryByName(name: string): Promise<Category> {
+  try {
+    const categories = await client.get({
+      endpoint: MICROCMS_CATEGORIES_ENDPOINT,
+      queries: { filters: `name[equals]${name}` },
+    });
+    return categories.contents[0];
+  } catch (error) {
+    console.error("指定のカテゴリの取得に失敗しました。", error);
+    throw error;
+  }
+}
+
+export async function getBlogListByCategoryName(
+  name: string
+): Promise<BlogPostList> {
+  try {
+    const category = await getCategoryByName(name);
+    const blogs = await client.get({
+      endpoint: MICROCMS_BLOGS_ENDPOINT,
+      queries: { filters: `category[equals]${category.id}` },
+    });
+    return blogs.contents;
+  } catch (error) {
+    console.error("指定のカテゴリのブログ一覧の取得に失敗しました。", error);
     throw error;
   }
 }
--- types/microcms.ts
+++ types/microcms.ts
@@ -1,9 +1,12 @@
+export type BlogPostList = BlogPost[];
+
 export type BlogPost = {
   id: string;
   createdAt: string;
   updatedAt: string;
   publishedAt: string;
   revisedAt: string;
+  slug?: string;
   title?: string;
   content?: string;
   eyecatch?: {
@@ -11,13 +14,16 @@ export type BlogPost = {
     height: number;
     width: number;
   };
-  category?: {
-    id: string;
-    createdAt: string;
-    updatedAt: string;
-    publishedAt: string;
-    revisedAt: string;
-    name: string;
-  };
-  slug?: string;
+  category?: Category;
 };
+
+export type Category = {
+  id: string;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  revisedAt: string;
+  name: string;
+};
+
+export type CategoryList = Category[];
