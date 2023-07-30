b5dc6f43254651892ca83b7a0e71c2f772681151
Nicolas Giaconia <73022145+nicolas-webdev@users.noreply.github.com>
2023-07-30 18:40:20 +0900

    Edit footer links

--- app/(components)/Footer.tsx
+++ app/(components)/Footer.tsx
@@ -8,13 +8,13 @@ const Footer = () => {
           </h3>
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
             <a
-              href="https://nextjs.org/docs/basic-features/pages"
+              href="https://nextjs.org/docs/app/building-your-application"
               className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
             >
               ドキュメント
             </a>
             <a
-              href="https://github.com/vercel/next.js/tree/canary/examples/blog-starter"
+              href="https://github.com/vercel/next.js/tree/canary/examples/"
               className="mx-3 font-bold hover:underline"
             >
               GitHubで見る
