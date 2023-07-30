1db49dcd128571a4130d3134f92c624fd1805e8a
Nicolas Giaconia <73022145+nicolas-webdev@users.noreply.github.com>
2023-07-31 00:42:54 +0900

    Clean up metadata

---
+++ app/favicon.tsx
@@ -0,0 +1,36 @@
+import { SITE_TITLE } from "@/config";
+import { ImageResponse } from "next/server";
+
+export const runtime = "edge";
+
+export const size = {
+  width: 32,
+  height: 32,
+};
+export const contentType = "image/png";
+
+export default function Icon() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 24,
+          background: "black",
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          color: "white",
+          borderRadius: "100%",
+        }}
+      >
+        {SITE_TITLE.slice(0, 1) || "N"}
+      </div>
+    ),
+    // ImageResponse options
+    {
+      ...size,
+    }
+  );
+}
