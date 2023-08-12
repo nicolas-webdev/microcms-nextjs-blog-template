60c9567682c26e674f0bdce04b7de46494e39d88
Nicolas Giaconia <73022145+nicolas-webdev@users.noreply.github.com>
2023-08-12 18:53:24 +0900

    Push & hope it still works after the refactorings

--- config.ts
+++ config.ts
@@ -1,11 +1,10 @@
-// URL関連
+// サイト情報に関する設定
 export const SITE_URL =
   process.env.NODE_ENV === "production"
     ? process.env.SITE_URL || ""
     : process.env.VERCEL_URL || "http://localhost:3000";
-export const SITE_HOSTNAME = process.env.SITE_HOSTNAME || "example.com";
 
-// メタデータ関連
+export const SITE_HOSTNAME = process.env.SITE_HOSTNAME || "example.com";
 export const SITE_TITLE = process.env.SITE_TITLE || "MicroNext";
 export const SITE_DESCRIPTION =
   process.env.SITE_DESCRIPTION ||
@@ -16,7 +15,7 @@ export const SITE_AUTHOR_URL =
   process.env.SITE_AUTHOR_URL || "https://twitter.com/_xkime";
 export const SITE_IMAGE = process.env.SITE_IMAGE || "/api/og?title=MicroNext";
 
-// MicroCMS関連
+// MicroCMSの設定
 export const MICROCMS_SERVICE_DOMAIN =
   process.env.MICROCMS_SERVICE_DOMAIN || "";
 export const MICROCMS_API_KEY = process.env.MICROCMS_API_KEY || "";
@@ -25,10 +24,10 @@ export const MICROCMS_BLOGS_ENDPOINT =
 export const MICROCMS_CATEGORIES_ENDPOINT =
   process.env.MICROCMS_CATEGORIES_ENDPOINT || "categories";
 
-// ISR関連
+// ISR(Incremental Static Regeneration)の設定
 export const REVALIDATE_SECRET = process.env.REVALIDATE_SECRET || "secret";
 export const REVALIDATE_INTERVAL =
   Number(process.env.REVALIDATE_INTERVAL) || 86400;
 
-// DRAFT関連
+// 下書き関連の設定
 export const DRAFT_SECRET = process.env.DRAFT_SECRET || "draft";
