1a1e4f5455ca7c272aa59c636e1f20ef79794851
Nicolas Giaconia <73022145+nicolas-webdev@users.noreply.github.com>
2023-08-12 17:30:39 +0900

    Add a Skeleton

---
+++ app/blog/preview/[id]/[draftKey]/loading.tsx
@@ -0,0 +1,63 @@
+export default function Loading() {
+  // Or a custom loading skeleton component
+  return (
+    <div role="status" className="space-y-2.5 animate-pulse max-w-lg">
+      <div className="flex items-center w-full space-x-2">
+        <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-32"></div>
+        <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24"></div>
+        <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
+      </div>
+      <div className="flex items-center w-full space-x-2 max-w-[440px]">
+        <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-32"></div>
+        <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24"></div>
+        <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-full"></div>
+      </div>
+      <div className="flex items-center w-full space-x-2 max-w-[360px]">
+        <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
+        <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-80"></div>
+        <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
+      </div>
+      <div className="flex items-center w-full space-x-2 max-w-[440px]">
+        <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-32"></div>
+        <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24"></div>
+        <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-full"></div>
+      </div>
+      <div className="flex items-center w-full space-x-2 max-w-[360px]">
+        <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
+        <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-80"></div>
+        <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
+      </div>
+      <div className="flex items-center w-full space-x-2 max-w-[400px]">
+        <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
+        <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-80"></div>
+        <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
+      </div>
+      <div className="flex items-center w-full space-x-2 max-w-[480px]">
+        <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-full"></div>
+        <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
+        <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24"></div>
+      </div>
+      <div className="flex items-center w-full space-x-2 max-w-[440px]">
+        <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-32"></div>
+        <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24"></div>
+        <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-full"></div>
+      </div>
+      <div className="flex items-center w-full space-x-2 max-w-[360px]">
+        <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
+        <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-80"></div>
+        <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
+      </div>
+      <div className="flex items-center w-full space-x-2 max-w-[440px]">
+        <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-32"></div>
+        <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24"></div>
+        <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-full"></div>
+      </div>
+      <div className="flex items-center w-full space-x-2 max-w-[360px]">
+        <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
+        <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-80"></div>
+        <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
+      </div>
+      <span className="sr-only">Loading...</span>
+    </div>
+  );
+}
