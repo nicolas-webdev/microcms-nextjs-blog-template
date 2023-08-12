60d786a35a5c1047ab76b327220502d8646ad692
Nicolas Giaconia <73022145+nicolas-webdev@users.noreply.github.com>
2023-08-12 19:34:23 +0900

    Clean up PostBody component

--- app/(components)/PostBody.tsx
+++ app/(components)/PostBody.tsx
@@ -9,16 +9,14 @@ import parse, {
   domToReact,
 } from "html-react-parser";
 
-// タグタイプの定数
 const TAG_TYPE = "tag";
 const IMG_TAG = "img";
 const A_TAG = "a";
 const HOSTNAME = SITE_HOSTNAME;
 
-// imgタグを処理するためのヘルパー関数
+// 画像タグの処理
 const handleImageTag = (element: Element) => {
   if (element.attribs?.src) {
-    // 画像にheightとwidthの属性がある場合、アスペクト比を維持
     return element.attribs.height && element.attribs.width ? (
       <Image
         src={element.attribs.src}
@@ -27,7 +25,6 @@ const handleImageTag = (element: Element) => {
         height={parseInt(element.attribs.height)}
       />
     ) : (
-      // 画像にheightやwidthの属性がない場合、layout fillを使用
       <div className="relative aspect-video">
         <Image
           src={element.attribs.src}
@@ -40,32 +37,28 @@ const handleImageTag = (element: Element) => {
   }
 };
 
-// aタグを処理するためのヘルパー関数
+// リンクタグの処理
 const handleATag = (element: Element, options: HTMLReactParserOptions) => {
   if (element.attribs?.href) {
     const href = element.attribs.href;
-    const url = new URL(href, SITE_URL); // ベースURL
+    const url = new URL(href, SITE_URL);
     const linkText = element.children?.length
       ? domToReact(element.children, options)
       : null;
 
-    // リンクが内部の場合、Next.jsのLinkコンポーネントを使用
-    if (url.hostname === HOSTNAME) {
-      return <Link href={href}>{linkText}</Link>;
-    } else {
-      // リンクが外部の場合、target="_blank"を追加
-      return (
-        <a href={href} rel="noopener noreferrer" target="_blank">
-          {linkText}
-        </a>
-      );
-    }
+    return url.hostname === HOSTNAME ? (
+      <Link href={href}>{linkText}</Link>
+    ) : (
+      <a href={href} rel="noopener noreferrer" target="_blank">
+        {linkText}
+      </a>
+    );
   }
 };
 
+// HTMLのパース
 const parseHTML = (htmlContent: string) => {
   const { window } = new JSDOM("");
-  // DOMPurifyを使用してHTMLをサニタイズ
   const DOMPurify = createDOMPurify(window);
   const clean = DOMPurify.sanitize(htmlContent);
 
@@ -73,15 +66,12 @@ const parseHTML = (htmlContent: string) => {
     replace: (domNode) => {
       if (domNode.type === TAG_TYPE) {
         const element = domNode as Element;
-        // imgタグを処理
         if (element.name === IMG_TAG) return handleImageTag(element);
-        // aタグを処理
         if (element.name === A_TAG) return handleATag(element, options);
       }
     },
   };
 
-  // サニタイズしたHTMLをパース
   return parse(clean, options);
 };
 
@@ -89,10 +79,9 @@ type PostBodyProps = {
   htmlContent: string;
 };
 
-// 投稿本文を表示するコンポーネント
+// 投稿の本文コンポーネント
 const PostBody = ({ htmlContent }: PostBodyProps) => {
   const parsedHtmlContents = parseHTML(htmlContent);
-  // TailwindCSSのproseプラグインを使用してブログコンテンツをスタイル付け
   return (
     <main className="container mx-auto px-5 prose prose-slate lg:prose-xl dark:prose-invert prose-img:rounded-sm prose-a:text-blue-600">
       {parsedHtmlContents}
